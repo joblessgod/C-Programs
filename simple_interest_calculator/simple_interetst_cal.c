aea6d3219da1ce5f22c4fab05844fe07c28d9389
JobLessGod <iamjoblessgod@gmail.com>
2024-12-10 00:45:56 +0545

    simple_interest_cal code

---
+++ simple_interest_calculator/simple_interetst_cal.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+	float P,T,R,SimpleInterest;
+	printf("Enter Principle(NPR), Time(Yeras), Rate(Percentage): ");
+	scanf("%f%f%f",&P,&T,&R);
+
+	SimpleInterest = P*T*R/100;
+	
+
+	printf("\nPrinciple: NPR %.2f\nTime: %.f Year\nRate: %.f % \nSimple Interest: NPR %.2f\n",P,T,R,SimpleInterest);
+
+	getchar();
+	getchar();
+	return 0;
+
+}
