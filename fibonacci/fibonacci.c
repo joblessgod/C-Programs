da967e1d3e9e322e320379f15887b8078cf14a60
JobLessGod <iamjoblessgod@gmail.com>
2025-02-19 00:50:00 +0545

    Designed Result Part, and added user input up to count

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
 int main () {
 
-	int a = 0,b = 1, count=2;
-	printf("%d %d",a,b);
+	int a = 0,b = 1, count;
+	int number = 0;
 	
-	while (count <= 10){
+	printf("Enter counting up to digit: ");
+	scanf("%d", &number);
+	
+	//Result design only!
+	printf("\n");
+	printf("Fabinacci Series up to '%d'",number);
+	printf("\nAns: ");
+	
+	printf("%d, %d",a,b); // print initial values
+
+	while (count <= number){
 		int c = a + b;
-		printf(",%d",c);
+		printf(", %d",c);
 		a = b;
 		b = c;
 		count = count+1;
 	}
+	printf("\n");
+	getchar();
+	getchar();
 	return 0;
 }
