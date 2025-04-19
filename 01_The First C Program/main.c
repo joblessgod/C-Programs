fd92b832b3c4791c6965d98d50045926b254cf43
JobLessGod <iamjoblessgod@gmail.com>
2025-04-19 23:21:12 +0545

    first code from book, "Let Us"

---
+++ 01_The First C Program/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // variables
+    int p, n;
+    float r, si;
+
+    // values
+    p = 100;
+    n = 3;
+    r = 8.5;
+
+    // formula
+    si = p * n * r / 100;
+    printf("%f\n", si);
+    return 0;
+}
