a3c75175ef5a2cb682f07c5e2901d38a835b59fa
JobLessGod <iamjoblessgod@gmail.com>
2025-05-01 00:23:37 +0545

    printed all 0-255 character lab3

---
+++ Assignment/Lab #2/Problem_2.3.c
@@ -0,0 +1,16 @@
+/* Question:
+     Write a C program to print all characters corresponding to ASCII values from 1 to
+     255.
+*/
+
+#include <stdio.h>
+int main()
+{
+     int i;
+     for (i = 0; i < 255; i++)
+     {
+          printf("> ASCII Num: %d\n  Character: %c\n\n", i, i);
+     }
+
+     return 0;
+}
