5f95a34642f44456f8b31f0976ed3f677bda8b4c
JobLessGod <iamjoblessgod@gmail.com>
2025-04-30 19:37:28 +0545

    2,4 problem of lab #2 solved

---
+++ Assignment/Lab #2/Problem_2.2.c
@@ -0,0 +1,24 @@
+/* Question:
+     Write a C program to display the size (in bytes) of various data types like int, float,
+     char, double, and string using the sizeof operator.
+*/
+
+#include <stdio.h>
+int main()
+{
+     int intSize = sizeof(int);
+     int floatSize = sizeof(float);
+     int charSize = sizeof(char);
+     int doubleSize = sizeof(double);
+     int stringSize = sizeof("");
+
+     printf("Size of \'int\' = %d byte\n", intSize);
+     printf("Size of \'float\' = %d byte\n", floatSize);
+     printf("Size of \'char\' = %d byte\n", charSize);
+     printf("Size of \'double\' = %d byte\n", doubleSize);
+     printf("Size of \'string\' = %d byte\n", stringSize);
+     scanf("%d", &intSize);
+     printf("Size of \'int\' = %d byte\n", intSize);
+
+     return 0;
+}
---
+++ Assignment/Lab #2/Problem_2.4.c
@@ -0,0 +1,40 @@
+/* Question:
+     Write a C program to demonstrate and differentiate between global, local, static,
+     and non-static variables by printing their values from different functions.
+*/
+
+#include <stdio.h>
+int a;     // globle variable a and initialize
+int b = 0; // mark it as false
+
+void assignValue()
+{
+     printf("\nPrint from PRIVATE Function\n");
+
+     a = 200; // assign value of b
+     b = 1;   // mark it as true
+
+     printf("\tValue of a: %d\n", a);
+     printf("\tValue of b: %d\n", b);
+}
+
+int main()
+{
+     // assignValue();
+     // if we make it visible it will update the value of b and PUBLIC fuction's ifelse will be trigger as 'true' and return updated value of b from that PRIVATE function.
+
+     a = 100; // assign value of b
+
+     printf("\nPrint from PUBLIC Function\n");
+     printf("\tValue of a: %d\n", a);
+
+     if (b)
+     {
+          printf("\tValue of b: %d\n", b);
+     }
+     else
+     {
+          printf("\t\'b\' is not assigned yet!\n");
+     }
+     return 0;
+}
