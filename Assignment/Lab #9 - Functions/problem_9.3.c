ff2925aa7a5fd575c6facda911a8700f9bab69df
JobLessGod <iamjoblessgod@gmail.com>
2025-07-08 01:54:07 +0545

    ✅ "Lab #9 - Function" : Well organized

--- Assignment/Lab #9 - Functions/problem_9.3.c
+++ Assignment/Lab #9 - Functions/problem_9.3.c
@@ -8,23 +8,22 @@
 #define GREEN "\033[32m"
 #define RED "\033[31m"
 
-int isPrime(int num){
-  if (num <= 1) return printf(RED"%d is not a prime number.\n"RESET, num);
-  for (int i = 2; i * i <= num; i++){
-    if (num % i == 0) return 0;
-  }
-  return 1;
+int isPrime(int num) {
+    if (num <= 1) return 0; // 0 and 1 are not prime numbers
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0; // Not a prime number
+    }
+    return 1; // Is a prime number
 }
 
-int main()
-{
-  int inpNum;
+int main() {
+  int number;
 
   printf("Enter a number: ");
-  scanf("%d", &inpNum);
-  if (isPrime(inpNum))
-    printf(GREEN "%d is a prime number.\n" RESET, inpNum);
+  scanf("%d", &number);
+  if (isPrime(number))
+    printf(GREEN "%d is a prime number.\n" RESET, number);
   else
-    printf(RED "%d is not a prime number.\n" RESET, inpNum);
+    printf(RED "%d is not a prime number.\n" RESET, number);
   return 0;
 }
