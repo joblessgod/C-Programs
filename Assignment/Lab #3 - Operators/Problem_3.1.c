8f86fb3700aa5b38a701f62e374e50ca9b809126
JobLessGod <iamjoblessgod@gmail.com>
2025-07-07 23:19:56 +0545

    ✅ "Lab #3 - Operators" : Organized

--- Assignment/Lab #3 - Operators/Problem_3.1.c
+++ Assignment/Lab #3 - Operators/Problem_3.1.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <conio.h>
 #include <windows.h>
 
 // Color codes
@@ -12,14 +13,16 @@
 int main()
 {
 	SetConsoleTitle("Lab #3.1"); // to change the title of console/terminal
+     system("cls");
 	
      int a = 2,b =20;
      printf(GREEN"Default value"RESET"\n a: %d\n b: %d",a,b);
      a++;
      b--;
-     printf(GREEN"\n\nValue Initial"RESET"\n a++ : %d (increment)\n b-- : %d (decrement)",a,b);
-     printf(GREEN"\n\nValue Before"RESET"\n a++ : %d (same as prev)\n b-- : %d (same as prev)",a++,b--);
-     printf(GREEN"\n\nValue Before"RESET"\n a++ : %d (increment)\n b-- : %d (decrement)\n",a,b);
-
+     printf(GREEN"\n\nValue Initial Prefix"RESET"\n a++ : %d (increment)\n b-- : %d (decrement)",a,b);
+     printf(GREEN"\n\nValue Before Postfix"RESET"\n a++ : %d (same as prev)\n b-- : %d (same as prev)",a++,b--);
+     printf(GREEN"\n\nValue After Postfix"RESET"\n a++ : %d (increment)\n b-- : %d (decrement)\n",a,b);
+     printf("\nPress any key to exit...");
+     getch();
      return 0;
 }
