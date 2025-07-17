e492b13e8cb2ee1d1c46dbf5004e1d91894053f3
JobLessGod <iamjoblessgod@gmail.com>
2025-07-18 00:51:03 +0545

    finally done

--- Exam Preparation/text_pattern_print.c
+++ Exam Preparation/text_pattern_print.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-    char name[] = "POKHARA";
-    int len = strlen(name);
 
-    for (int i = 0; i <= 4; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            printf("  ");
+int main() {
+    char str[] = "POKHARA";
+    int len = strlen(str);
+
+    for (int i = 0; i < 4; i++) {
+        // Print leading spaces (each space = 2 characters to align with letters + space)
+        for (int s = 0; s < i * 2; s++) {
+            printf(" ");
         }
 
-        for (int j = 0; j <= len / 2 + 1 - i; j++)
-        {
-            printf("%c ", name[j + i]);
+        // Print the characters
+        for (int j = i; j < len - i; j++) {
+            printf("%c", str[j]);
+            if (j != len - i - 1) {
+                printf(" ");
+            }
         }
 
-        for (int j = len / 2 + 2; j < len-i; j++)
-        {
-            printf("%c ", name[j]);
-        }
         printf("\n");
     }
 
